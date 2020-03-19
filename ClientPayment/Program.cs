d159a407a6961f2363d4ff55a273bd464c4a5041
Vinod <vvinod@internal.tavisca.com>
2020-03-19 12:59:35 +0530

    Add project files.

---
+++ ClientPayment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPayment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentInterfaceClient client = new PaymentInterfaceClient();
+            ((IContextChannel)client.InnerChannel).OperationTimeout = new TimeSpan(0, 30, 0);
+            PaymentInterface.BusinessComponents.AuthorizeCreditCardResponse response = (client.AuthorizeCreditCardAsync(new PaymentInterface.BusinessComponents.AuthorizeCreditCardRequest(), "")).Result;
+        }
+    }
+}
